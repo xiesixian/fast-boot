37e0e9d942bcea06523173a1194618afac4f1dc5
sixian.xie <sixian.xie@qq.com>
2020-08-13 12:00:28 +0800

    no message

--- src/main/java/com/xiesx/fastboot/support/schedule/ScheduleHelper.java
+++ src/main/java/com/xiesx/fastboot/support/schedule/ScheduleHelper.java
@@ -203,18 +203,16 @@ public class ScheduleHelper {
             String job_name = "动态任务调度";
             System.out.println("【系统启动】开始(每1秒输出一次)...");
             ScheduleHelper.addJob(job_name, SimpleJobSchedule.class, "0/1 * * * * ?", map);
-            Thread.sleep(5000);
             System.out.println("【修改时间】开始(每2秒输出一次)...");
-            ScheduleHelper.modifyJobTime(job_name, "10/2 * * * * ?");
-
-            Thread.sleep(6000);
+            ScheduleHelper.modifyJobTime(job_name, "0/2 * * * * ?");
+            Thread.sleep(5000);
             System.out.println("【移除定时】开始...");
             ScheduleHelper.removeJob(job_name);
             System.out.println("【移除定时】成功");
 
-            System.out.println("【再次添加定时任务】开始(每10秒输出一次)...");
-            ScheduleHelper.addJob(job_name, SimpleJobSchedule.class, "*/10 * * * * ?", map);
-            Thread.sleep(6000);
+            System.out.println("【再次添加定时任务】开始(每5秒输出一次)...");
+            ScheduleHelper.addJob(job_name, SimpleJobSchedule.class, "*/5 * * * * ?", map);
+            Thread.sleep(11000);
             System.out.println("【移除定时】开始...");
             ScheduleHelper.removeJob(job_name);
             System.out.println("【移除定时】成功");
--- src/test/java/com/xiesx/fastboot/http/HttpTest.java
+++ src/test/java/com/xiesx/fastboot/http/HttpTest.java
@@ -1,14 +1,8 @@
 package com.xiesx.fastboot.http;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.xiesx.fastboot.base.BaseTest;
-
-import io.restassured.response.Response;
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * @title SimpleTest.java
@@ -16,21 +10,14 @@ import lombok.extern.slf4j.Slf4j;
  * @author Sixian.xie
  * @date 2018年12月21日 下午6:16:35
  */
-@Slf4j
 @RunWith(SpringJUnit4ClassRunner.class)
 // @SpringBootTest(classes = ZeduApplication.class, webEnvironment =
 // SpringBootTest.WebEnvironment.RANDOM_PORT)
 // @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class HttpTest extends BaseTest {
-
-    /**
-     * 传参
-     */
+public class HttpTest {
+ 
     @Test
     public void str() {
-        Response response = httpGet("/simple/testParam1?data=1");
-        log.info("response {}", response.getBody().asString());
-        String data = response.jsonPath().getString("data");
-        Assert.assertTrue(data.equals("1"));
+      
     }
 }

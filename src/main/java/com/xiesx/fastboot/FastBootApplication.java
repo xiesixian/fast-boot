33bbb3738022b5bd2c4a7f5bc4fa4e2d12695608
sixian.xie <sixian.xie@qq.com>
2020-08-24 18:01:59 +0800

    no message

--- src/main/java/com/xiesx/fastboot/FastBootApplication.java
+++ src/main/java/com/xiesx/fastboot/FastBootApplication.java
@@ -1,11 +1,5 @@
 package com.xiesx.fastboot;
 
-import org.springframework.boot.Banner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.validation.ValidationAutoConfiguration;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -15,13 +9,14 @@ import lombok.extern.slf4j.Slf4j;
  * @date 2020年1月6日下午2:06:58
  */
 @Slf4j
-@SpringBootApplication(exclude = ValidationAutoConfiguration.class)
-public class FastBootApplication extends SpringBootServletInitializer {
+// @SpringBootApplication(exclude = ValidationAutoConfiguration.class)
+// public class FastBootApplication extends SpringBootServletInitializer {
+public class FastBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(FastBootApplication.class);
-        app.setBannerMode(Banner.Mode.CONSOLE);
-        app.run(args);
+        // SpringApplication app = new SpringApplication(FastBootApplication.class);
+        // app.setBannerMode(Banner.Mode.CONSOLE);
+        // app.run(args);
         log.info("Started FastBootApplication 启动成功");
     }
 }
